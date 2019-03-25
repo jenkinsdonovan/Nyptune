acc6bfab1f17c21e747ab13ddcfb90913fe042c3
Donovan Jenkins <donovan.jenkins@student.nmt.edu>
2019-03-24 20:35:52 -0600

    last change

--- index.php
+++ index.php
@@ -1,64 +1,67 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="style.css">
-<link rel="shortcut icon" type="image/ico" href="images/favicon.ico"/>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<title>Nyptune Web Player | Landing</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="shortcut icon" type="image/ico" href="images/favicon.ico"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<title>Nyptune Web Player | Landing</title>
 </head>
 
-
-
 <body>
-
 <div class="navbar">
-  <a href="index.html">Nyptune</a>
-  <div class="container">
-    <div class="col">
-		<?php
+	<a href="index.html">Nyptune</a>
+	<div class="logincontainer"><?php
 		if(!isset($_SESSION['logged']) || $_SESSION['logged'] == FALSE) {
-		echo '
-        <form action="login.php" method="post" style="width: 100%; display: inline-block;">
-        	<input type="text" name="username" placeholder="Username" required>
-        	<input type="password" name="password" placeholder="Password" required>
-        	<input type="submit" value="Login">
-        </form>
-		';
+			echo '<form action="login.php" method="post" class="loginform">
+				 <input type="text" name="username" placeholder="Username" required>
+				 <input type="password" name="password" placeholder="Password" required>
+				 <input type="submit" value="Login"></form>';
 		} else {
-		echo '
-		<form action="logout.php" method="post">
-			<input type="submit" value="Logout"/>
-		</form>
-		';
+			echo '<form action="logout.php" method="post">
+				 <input type="submit" value="Logout"/></form>';
 		}
-		?>
-      </div>
-    </div>
+	?></div>
   <input type="text2" id="search" name="search" oninput="search()" placeholder="Search for artists, songs, podcasts..."> 
 </div>
-</div>
-
 
 <div class="main">
+	<div class="sidenav">
+		<button onclick="home();">Home</button>
+		<button onclick="upload();">Upload</button>
+		<button onclick="playlist();">Playlists</button>
+		<button onclick="settings();">Settings</button>
+	</div>
 
+	<div class="maincontainer" id="maincontainer"/>
 
-  <div class="sidenav">
-  <button onclick="home();">Home</button>
-  <button onclick="upload();">Upload</button>
-  <button onclick="playlist();">Playlists</button>
-  <button onclick="settings();">Settings</button>
-  </div>
+	<script> 
+		/* to be loaded when the page loads */
+		$(document).ready(function() {
+			$(".maincontainer").html("home");
+		});
 
-  <div class="maincontainer" id="maincontainer">
-
-  </div>
-
-  
+		/* button functions. Will eventually use ajax to call php scripts */
+		function search() {
+			var x = document.getElementById("search").value;
+			document.getElementById("maincontainer").innerHTML = "Search: " + x;
+		}
+		function home() {
+			$(".maincontainer").html("home");
+		}
+		function upload() {
+			$(".maincontainer").html("upload");
+		}
+		function playlist() {
+			$(".maincontainer").html("playlist");
+		}
+		function settings() {
+			$(".maincontainer").html("settings");
+		}
+	</script>
 </div>
 </body>
 
@@ -67,30 +70,6 @@ session_start();
 Your browser does not support the audio element.
 </audio>
 
-<script> 
-	/* to be loaded when the page loads */
-	$(document).ready(function(){
-		$(".maincontainer").html("home");
-	});
 
-	/* button functions. Will eventually use ajax to call php scripts */
-	function search() {
-		var x = document.getElementById("search").value;
-		document.getElementById("maincontainer").innerHTML = "Search: " + x;
-		/*$(".maincontainer").html("search");*/
-	}
-	function home() {
-		$(".maincontainer").html("home");
-	}
-	function upload() {
-		$(".maincontainer").html("upload");
-	}
-	function playlist() {
-		$(".maincontainer").html("playlist");
-	}
-	function settings() {
-		$(".maincontainer").html("settings");
-	}
-</script>
 
 </html>
